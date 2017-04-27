aaad3ed24bfd48fc01f145e92d1fa1e2b442c697
Jun Fritz and Phat Nguyen <phat@>
2017-04-27 09:05:23 -0700

    add seperate sections for back end and front end logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
+//Back End Logic
+var vote = function(age) {
+  //if legal do this
+ if (age >= 18) {
+   $("#over-18").show();
+ }
+ //if minor do this
+ else {
+   $("#under-18").show();
+ }
+};
+
+
+
+//Front End
 $(document).ready(function() {
   var age = parseInt(prompt("How old are you?"));
-
-  //if legal do this
-  if (age >= 18) {
-    $("#over-18").show();
-  }
-  //if minor do this
-  else {
-    $("#under-18").show();
-  }
+  var legal = vote(age);
 });
